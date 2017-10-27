b80ceeb43ec57fb9b7dca80b58235f2aedbe8d00
Valeriy Kolomiets <vel202007@gmail.com>
2017-10-27 06:42:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+viazushki
+=========
+
+A Symfony project created on October 26, 2017, 10:36 pm.
---
+++ src/ViazushkiBundle/ViazushkiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ViazushkiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ViazushkiBundle extends Bundle
+{
+}
