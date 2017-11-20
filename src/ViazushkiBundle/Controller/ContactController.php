d38964c3ee18f360b0bb4052634d3d8a74898da6
Valeriy Kolomiets <vel202007@gmail.com>
2017-11-20 22:03:02 +0200

    Added SendingMessage service and ContactType

--- src/ViazushkiBundle/Controller/ContactController.php
+++ src/ViazushkiBundle/Controller/ContactController.php
@@ -4,11 +4,42 @@ namespace ViazushkiBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use ViazushkiBundle\Entity\Contact;
+use ViazushkiBundle\Form\Type\ContactType;
 
 class ContactController extends Controller
 {
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('@Viazushki/Contact/contact.html.twig');
+        $contact = new Contact();
+        $contactForm = $this->createForm(ContactType::class, $contact);
+
+
+        if ($request->isMethod('post')) {
+            $contactForm->handleRequest($request);
+
+            if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+
+                $sendingMessage = $this->container->get('viazushki.sending_message');
+                $sendingMessage->setEmail($contactForm->get('email')->getData());
+                $sendingMessage->setBody(
+                    $this->renderView('@Viazushki/Email/contactEmail.html.twig', [
+                        'name' => $contactForm->get('name')->getData(),
+                        'email' => $contactForm->get('email')->getData(),
+                        'message' => $contactForm->get('text')->getData(),
+                    ])
+                );
+
+                if($sendingMessage->send()) {
+                    return $this->redirectToRoute('viazushki_contacts');
+                }
+            }
+
+        }
+
+        return $this->render('@Viazushki/Contact/contact.html.twig', [
+            'contactForm' => $contactForm->createView(),
+        ]);
     }
 }
---
+++ src/ViazushkiBundle/Entity/Contact.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace ViazushkiBundle\Entity;
+
+
+class Contact
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $text;
+
+    /**
+     * @var string
+     */
+    private $date;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param string $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+
+}
