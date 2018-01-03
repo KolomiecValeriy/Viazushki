20b74247b5c7143cfe813e315a5350ba59a6dcb7
Valeriy Kolomiets <vel202007@gmail.com>
2018-01-03 18:55:09 +0200

    Sending commints with ajax

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -21,19 +21,7 @@ $(document).ready(function () {
         })
     });
 
-    $('[data-comment-reply]').on('click', function (event) {
-       var currentCommentId = $(event.currentTarget).attr('data-comment-reply');
-
-        $('[data-comment-reply-form]').each(function (i, current) {
-            if ($(current).attr('data-comment-reply-form') === currentCommentId) {
-                $(current).toggle(300);
-            }
-        });
-    });
-
-    $('[data-comment-reply-all]').on('click', function (event) {
-        $(event.currentTarget).closest('li').find('ul').toggle(300);
-    });
+    initComments();
 });
 
 // Portfolio
@@ -77,3 +65,38 @@ $(window).load(function () {
 $(function () {
     $('#hornavmenu').slicknav();
 });
+
+// Functions
+function initComments() {
+    $('[data-comment-reply]').on('click', function (event) {
+        var currentCommentId = $(event.currentTarget).attr('data-comment-reply');
+
+        $('[data-comment-reply-form]').each(function (i, current) {
+            if ($(current).attr('data-comment-reply-form') === currentCommentId) {
+                $(current).toggle(300);
+            }
+        });
+    });
+
+    $('[data-comment-reply-all]').on('click', function (event) {
+        $(event.currentTarget).closest('li').find('ul').toggle(300);
+    });
+
+    $('.blog-recent-comments [type=submit]').on('click', function (event) {
+        event.preventDefault();
+        var form = $(event.currentTarget).closest('form');
+        var action = form.attr('action');
+        var commentId = action.match(/[\d+]*$/g);
+
+        $.ajax({
+            url: action,
+            method: 'POST',
+            data: form.serialize(),
+            success: function (data) {
+                $('.blog-recent-comments').html($(data).find('.blog-recent-comments'));
+                initComments();
+                $('[data-comment-reply-form='+commentId[0]+']').closest('li').find('ul').toggle(300);
+            }
+        });
+    });
+}
