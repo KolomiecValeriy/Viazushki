6417941045e7b766a4e01b5c6b0c56c2f1fba184
Valeriy <vel202007@gmail.com>
2018-05-12 00:43:25 +0300

    fixed user fixtures

--- src/ViazushkiBundle/DataFixtures/ORM/UserFixtures.php
+++ src/ViazushkiBundle/DataFixtures/ORM/UserFixtures.php
@@ -7,6 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Validator\Exception\ValidatorException;
 use ViazushkiBundle\Entity\User;
 
 class UserFixtures extends Fixture implements ContainerAwareInterface
@@ -21,38 +22,63 @@ class UserFixtures extends Fixture implements ContainerAwareInterface
     public function load(ObjectManager $manager)
     {
         $factory = $this->container->get('security.encoder_factory');
+        $validator = $this->container->get('validator');
 
         $admin = new User();
 
         $encoder = $factory->getEncoder($admin);
+        $password = $encoder->encodePassword('11111111', $admin);
         $admin
             ->setUsername('superAdmin')
-            ->setEmail('superAdmin@gmail.com')
-            ->setPassword($encoder->encodePassword('superAdmin', $admin))
+            ->setEmail('super.admin@example.com')
+            ->setPassword($password)
+            ->setPlainPassword($password)
             ->setIsActive(true)
             ->setRoles(['ROLE_SUPER_ADMIN'])
+            ->setSubscribe(false)
         ;
+
+        $violations = $validator->validate($admin);
+        if (count($violations) > 0) {
+            throw new ValidatorException($violations->get(0)->getMessage());
+        }
         $manager->persist($admin);
 
         $userManager = new User();
+        $password = $encoder->encodePassword('22222222', $userManager);
         $userManager
             ->setUsername('admin')
-            ->setEmail('admin@gmail.com')
-            ->setPassword($encoder->encodePassword('admin', $userManager))
+            ->setEmail('admin@example.com')
+            ->setPassword($password)
+            ->setPlainPassword($password)
             ->setIsActive(true)
             ->setRoles(['ROLE_ADMIN'])
+            ->setSubscribe(false)
         ;
+
+        $violations = $validator->validate($userManager);
+        if (count($violations) > 0) {
+            throw new ValidatorException($violations->get(0)->getMessage());
+        }
         $manager->persist($userManager);
 
         for ($i = 1; $i <= 10; $i++) {
             $user = new User();
+            $password = $encoder->encodePassword('22222222', $user);
             $user
                 ->setUsername('user'.$i)
-                ->setEmail( 'user'.$i.'@gmail.com')
-                ->setPassword($encoder->encodePassword('user'.$i, $user))
+                ->setEmail( 'user.'.$i.'@example.com')
+                ->setPassword($password)
+                ->setPlainPassword($password)
+                ->setIsActive(true)
                 ->setRoles(['ROLE_USER'])
+                ->setSubscribe(false)
             ;
 
+            $violations = $validator->validate($user);
+            if (count($violations) > 0) {
+                throw new ValidatorException($violations->get(0)->getMessage());
+            }
             $manager->persist($user);
             $this->setReference('user'.$i, $user);
         }
